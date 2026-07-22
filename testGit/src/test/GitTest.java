c7f1fb4a0d24e4229336ae73f3af2f15d2170dd9
KTZW33 <atsumit0708@gmail.com>
2026-07-22 14:05:39 +0900

    GitTest java内容更新

--- testGit/src/test/GitTest.java
+++ testGit/src/test/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 		System.out.println("Hello GitHub!!");
 		System.out.println("Good Morning GitHub!!");
 		System.out.println("Good Night GitHub!!");
+		System.out.println("Good Bye GitHub!!");
 	}
 		
 }
